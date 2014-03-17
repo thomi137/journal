1ddfcd09e8dc18af5f9fbda98d0ad85b86d4e3b1
thomi137 <thomas@prosser.ch>
2014-03-17 19:59:51 +0100

    Initial commit

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,9 +6,9 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_dev',
+			'dsn'        => 'mysql:host=localhost:8889;dbname=journal_dev',
 			'username'   => 'root',
-			'password'   => 'root',
+			'password'   => '',
 		),
 	),
 );
